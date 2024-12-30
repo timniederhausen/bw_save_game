2b16951a61ac1e4469a2018290093e2f6e2ee13d
Tim Niederhausen <tim@rnc-ag.de>
2024-12-30 01:11:54 +0100

    feat: Reverse-engineered more persistence key details

--- src/bw_save_game/persistence.py
+++ src/bw_save_game/persistence.py
@@ -41,21 +41,60 @@ class PersistencePropertyDefinition:
     default: object
 
 
-def parse_key_string(key: str):
-    dots = key.split(":", 3)
-    if len(dots) < 3:
-        version = 5
-    else:
-        version = int(dots[0])
-        dots = dots[1:]
+@dataclass
+class PersistenceKeyWithUniqueId:
+    version: int  # usually 7
+    family: PersistenceFamilyId
+    persona_id: int
+    definition_id: int
+    third: int  # TODO: unknown function so far
+    uid: int | None = None  # TODO: where do these come from?
 
-    family, family_data = dots
-    family_data = family_data.split("|" if version >= 6 else ".", 13)
-    return version, family, family_data
+    @staticmethod
+    def from_string(key: str):
+        dots = key.split(":", 3)
+        if len(dots) < 3:
+            version = 5
+            family = dots[0]
+            family_data = dots[1]
+        else:
+            version = int(dots[0])
+            family = dots[1]
+            family_data = dots[2]
 
+        family_data = family_data.split("|" if version >= 6 else ".", 13)
 
-def format_key_string(family: str, family_data: list, version: int = 7):
-    return f"{version}:{family}:{('|' if version >= 6 else '.').join(family_data)}"
+        persona = 0
+        uid = None
+        definition_id = 0
+        third = 0
+
+        if family_data:
+            persona = int(family_data[0])
+            family_data = family_data[1:]
+
+        if family_data and family_data[0].startswith("uid="):
+            uid = int(family_data[0][len("uid=") :])
+            family_data = family_data[1:]
+
+        if family_data:
+            definition_id = int(family_data[0])
+            family_data = family_data[1:]
+
+        if family_data:
+            third = int(family_data[0])
+            family_data = family_data[1:]
+
+        return PersistenceKeyWithUniqueId(version, PersistenceFamilyId[family], persona, definition_id, third, uid)
+
+    def __str__(self):
+        family_data = [str(self.persona_id)]
+        if self.uid is not None:
+            family_data.append(f"uid={self.uid}")
+        family_data.append(self.definition_id)
+        family_data.append(self.third)
+
+        return f"{self.version}:{self.family.name}:{('|' if self.version >= 6 else '.').join(family_data)}"
 
 
 def get_persisted_value(definition: dict, property_id: int, property_type: str, default_value: object):
