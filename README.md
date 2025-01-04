86f8683405174fbc7c1842e2ee9ec7b698578f6b
Tim Niederhausen <tim@rnc-ag.de>
2025-01-04 04:32:47 +0100

    chore: Prepare for release

--- README.md
+++ README.md
@@ -47,8 +47,7 @@ It is a good idea to update the hooks to the latest version::
 
 [![GNU GPLv3 Image](https://www.gnu.org/graphics/gplv3-127x51.png)][2]
 
-`bw_save_game` is Free Software: You can use, study, share and improve it at your
-will. Specifically you can redistribute and/or modify it under the terms of the
+`bw_save_game` is Free Software under the terms of the
 [GNU General Public License][3] as published by the Free Software Foundation,
 either version 3 of the License, or (at your option) any later version.
 
--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
+# bw-save-game - BioWare save game tools
+# Copyright (C) 2024 Tim Niederhausen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Website: https://github.com/timniederhausen/bw_save_game
+# -*- coding: utf-8 -*-
 """
     Setup file for bw_save_game.
     Use setup.cfg to configure your project.
-
-    This file was generated with PyScaffold 4.6.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: https://pyscaffold.org/
 """
 
 from setuptools import setup
