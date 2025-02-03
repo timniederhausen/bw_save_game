909f0d257eb14399fec1047d490af23ebea54915
Tim Niederhausen <tim@rnc-ag.de>
2025-02-03 04:07:38 +0100

    refactor!: Add more data files and move them to LFS

--- src/bw_save_game/veilguard/__init__.py
+++ src/bw_save_game/veilguard/__init__.py
@@ -8,16 +8,25 @@ from .persistence import *  # noqa: F401,F403
 from .scripts import *  # noqa: F401,F403
 from .types import *  # noqa: F401,F403
 
+_FILES = files("bw_save_game.data").joinpath("veilguard")
+
+
+def _load_data_file(name: str):
+    with _FILES.joinpath(f"{name}.json").open() as fp:
+        return json.load(fp)
+
+
 # from data files:
-_FILES = files("bw_save_game.data")
-ALL_ITEMS = json.loads(_FILES.joinpath("veilguard", "item_list.json").read_text("utf-8"))
-ALL_CURRENCIES = json.loads(_FILES.joinpath("veilguard", "currencies.json").read_text("utf-8"))
-ALL_SKILL_GRAPHS = json.loads(_FILES.joinpath("veilguard", "skill_graphs.json").read_text("utf-8"))
-ALL_COLLECTIBLES = json.loads(_FILES.joinpath("veilguard", "collectibles.json").read_text("utf-8"))
+ALL_ITEMS = _load_data_file("items")
+ALL_CURRENCIES = _load_data_file("currencies")
+ALL_SKILL_GRAPHS = _load_data_file("skill_graphs")
+ALL_COLLECTIBLES = _load_data_file("collectibles")
+ALL_QUESTS = _load_data_file("quests")
+ALL_PERSISTENCE = _load_data_file("persistence")
 
 # post-processing for data files:
 for item in ALL_ITEMS:
-    item["key"] = f"{item['name' or 'NO NAME']} ({item['id']})"
+    item["key"] = f"{item['name'] or 'NO NAME'} ({item['id']})"
     item["guid"] = UUID(item["guid"])
 
 SKILL_GRAPHS = {g["id"]: g for g in ALL_SKILL_GRAPHS}
